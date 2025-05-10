7b1cc4c069c164fc8f9352a115f161dc46b2550f
Sparsh Gulati <gulatisparsh0212@gmail.com>
2025-05-10 14:54:57 +0530

    added hero

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+// next.config.js (ESM syntax)
+export default {
+     
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: '**', // Allow images from any domain
+        },
+      ],
+      domains: ['cdn.prod.website-files.com'],
+    },
+  }
+  
+// next.config.js
---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import Hero from '../components/Hero';
+
+export default function Home() {
+  return (
+    <div className="w-[100%] bg-black ">
+      {/* <Navbar/> */}
+      <Hero />
+    </div>
+  )
+}
